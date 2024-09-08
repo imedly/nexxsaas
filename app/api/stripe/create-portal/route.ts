b09883063dd7118069c9a53a7efa16f13b0cb35c
El. Id. <foot4_6@hotmail.fr>
2024-09-08 10:12:35 +0100

    Update route.ts

--- app/api/stripe/create-portal/route.ts
+++ app/api/stripe/create-portal/route.ts
@@ -42,7 +42,7 @@ export async function POST(req: Request) {
     });
   }
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/";
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://nexxsaas.vercel.app";
 
   if (!customer?.id) {
     return new Response(JSON.stringify({ error: "Failed to get a customer id" }), { status: 500 });
